57ede2418b45e2777e596ecb169cdf7c9d3d7e35
rtsmc <ryan@trubey.net>
2022-05-04 19:09:26 -0700

    add drawing, add clear button functionality

--- drawingboard.js
+++ drawingboard.js
@@ -4,6 +4,7 @@ const drawBtn = document.getElementById('drawBtn');
 const gridBtn = document.getElementById('gridBtn');
 const sizeSlider = document.getElementById('sizeSlider');
 const sliderLabel = document.getElementById('sliderLabel');
+const clearBtn = document.getElementById('clearBtn');
 
 let pixelsPerSide = sizeSlider.value;
 let pixelHeight = 512/pixelsPerSide;
@@ -24,12 +25,10 @@ function setMode(mode){
     currentMode = mode;
 }
 
-for(let i = 0; i < (pixelsPerSide*pixelsPerSide); i++){
-    let newDiv = document.createElement("div");
-    newDiv.setAttribute("class", "pixel");
-    newDiv.style.height = pixelHeight + "px";
-    drawingBoard.appendChild(newDiv);
-    pixelArray.push(newDiv);
+updatePixels();
+
+colorPicker.oninput = () => {
+    currentColor = colorPicker.value;
 }
 
 sizeSlider.oninput = () => {
@@ -41,27 +40,7 @@ sizeSlider.oninput = () => {
     pixelHeight = 512/pixelsPerSide;
     sliderLabel.textContent = "Size: " + pixelsPerSide + "x" + pixelsPerSide;
 
-    pixelArray = [];
-    for(let i = 0; i < (pixelsPerSide*pixelsPerSide); i++){
-        let newDiv = document.createElement("div");
-        newDiv.setAttribute("class", "pixel");
-        newDiv.style.height = pixelHeight + "px";
-        drawingBoard.appendChild(newDiv);
-        pixelArray.push(newDiv);
-    }
-
-    drawingBoard.style = "grid-template-columns: repeat(" + pixelsPerSide + ", " + pixelHeight + "px)";
-    if(gridOn){
-        pixelArray.forEach(pixel => {
-            pixel.style.height = (pixelHeight - 2) + "px";
-            pixel.classList.add('grid');
-        });
-    } else {
-        pixelArray.forEach(pixel => {
-            pixel.style.height = (pixelHeight) + "px";
-            pixel.classList.remove('grid');
-        });
-    }
+    updatePixels();
 };
 
 gridBtn.addEventListener("click", () => {
@@ -78,4 +57,40 @@ gridBtn.addEventListener("click", () => {
         });
         gridOn = true;
     }
+});
+
+function updatePixels(){
+    pixelArray = [];
+    for(let i = 0; i < (pixelsPerSide*pixelsPerSide); i++){
+        let newDiv = document.createElement("div");
+        newDiv.setAttribute("class", "pixel");
+        newDiv.style.height = pixelHeight + "px";
+        drawingBoard.appendChild(newDiv);
+        pixelArray.push(newDiv);
+    }
+    pixelArray.forEach(pixel => {
+        pixel.addEventListener("click", () => {
+            pixel.style.backgroundColor = currentColor;
+        });
+    });
+
+    drawingBoard.style = "grid-template-columns: repeat(" + pixelsPerSide + ", " + pixelHeight + "px)";
+    if(gridOn){
+        pixelArray.forEach(pixel => {
+            pixel.style.height = (pixelHeight - 2) + "px";
+            pixel.classList.add('grid');
+        });
+    } else {
+        pixelArray.forEach(pixel => {
+            pixel.style.height = (pixelHeight) + "px";
+            pixel.classList.remove('grid');
+        });
+    }
+}
+
+clearBtn.addEventListener("click", () => {
+    pixelArray.forEach(pixel => {
+        drawingBoard.removeChild(pixel);
+    });
+    updatePixels();
 });
